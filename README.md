28b0d577c5155ec996b9dc7da180cd16e0e02743
Tate Peñaranda <tate@xenir.com.ar>
2016-03-07 16:31:45 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# LibTSC
+A small library to get data from personalized TSC Cards (Tarjeta Sin Contacto) using User ID (Documento Nacional de Identidad) and Card ID.
---
+++ src/MoniRos/LibTSC/getData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MoniRos\LibTSC;
+
+class HelloThere
+{
+    public function dummyFunction()
+    {
+        return 'Nothing at all...';
+    }
+}
